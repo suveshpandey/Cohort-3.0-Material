c170ccf01bcd7c7a81b79abe29d2ebfcefeb6a88
Suvesh Pandey <jpsuvesh29@gmail.com>
2024-09-15 19:03:55 +0530

    15th sept commit

--- assignments-main/week-4/middlewares/01-ratelimitter.js
+++ assignments-main/week-4/middlewares/01-ratelimitter.js
@@ -1,8 +1,5 @@
 // You have to create a middleware for rate limiting a users request based on their username passed in the header
 
-const express = require('express');
-const app = express();
-
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
 // If a user sends more than 5 requests in a single second, the server
@@ -11,17 +8,77 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
-let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
-});
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
-});
+let users = [];
+function generateId(){
+  let id = "";
+  while(id.length <= 5){
+    id += Math.floor(Math.random()*10);
+  }
+  return id;
+}
 
-module.exports = app;
+let userRequestLimit = {};
+
+setInterval(()=>{
+  console.log("resetting user request limit...");
+  userRequestLimit = {};
+}, 10000);
+
+app.use((req, res, next)=>{
+  const userId = req.headers['user-id'];
+
+  if(!userRequestLimit[userId]){
+    userRequestLimit[userId] = 0;
+  }
+  if(userRequestLimit[userId] >= 5){
+    console.log("ecceded the limit!")
+    return res.status(429).json({msg: 'Too many requests. Please try again later.'});
+  }
+  userRequestLimit[userId]++;
+  next();
+})
+app.post('/signup', (req, res)=>{
+  const username = req.body.username;
+  const userId = generateId();
+  console.log(userId);
+
+  users.push({
+    username,
+    userId,
+  })
+
+  res.status(201).json({
+    msg: "user added!",
+    username: username,
+    id: userId,
+  })
+})
+
+app.post('/signin', (req, res)=>{
+  const username = req.body.username;
+  const userId = req.headers['user-id'];
+  
+  console.log("username got :" + username);
+  console.log("user id got :" + userId);
+  const user = users.find(user => user.username == username && user.userId == userId);
+  if(user){
+    res.status(200).json({
+      msg: "user authenticated!",
+      username: username,
+      id: userId,
+    });
+  }
+  else{
+    res.status(400).json({msg: 'user not found.'});
+  }
+})
+
+
+
+
+app.listen(3000, ()=>console.log("server started!"));
--- sp.js
+++ sp.js
@@ -1,28 +1,29 @@
-// function clockFull(){
-//     const now = new Date();
+// You have to create a middleware for rate limiting a users request based on their username passed in the header
 
-//     let hours = now.getHours();
-//     let minutes = now.getMinutes();
-//     let seconds = now.getSeconds();
+const express = require('express');
+const app = express();
 
-//     console.log(`${hours} : ${minutes} : ${seconds}`)
-// }
-// setInterval(clock, 1000);
+// Your task is to create a global middleware (app.use) which will
+// rate limit the requests from a user to only 5 request per second
+// If a user sends more than 5 requests in a single second, the server
+// should block them with a 404.
+// User will be sending in their user id in the header as 'user-id'
+// You have been given a numberOfRequestsForUser object to start off with which
+// clears every one second
 
-function clock12Hours(){
-    let now = new Date();
+let numberOfRequestsForUser = {};
+setInterval(() => {
+    numberOfRequestsForUser = {};
+}, 1000)
 
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
+app.get('/user', function(req, res) {
+    res.status(200).json({ name: 'john' });
+});
 
-    let hours12 = hours%12;
-    if(hours12 == 0) hours12 = 12;
+app.post('/user', function(req, res) {
+    res.status(200).json({ msg: 'created dummy user' });
+});
 
-    let ampm;
-    if(hours >= 12) ampm = 'PM';
-    else ampm = 'AM';
-
-    console.log(`${hours12} : ${minutes} : ${seconds} ${ampm}`)
-}
-setInterval(clock12Hours, 1000);
+app.listen(3000, ()=>{
+    console.log("server started!")
+})
